06d3106fce472f7060d3d6cfbcb4586710ec4da6
CharlyBread <97170786+CharlyBread@users.noreply.github.com>
2022-04-10 16:45:19 +0300

    Update temp.cpp
    v?

--- temp.cpp
+++ temp.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-    cout << "Vortigont: Galanga" << endl;
+    double side;
+    
+    cin >> side; 
+    double square_P = side * 4;
+    double square_S = side * side;
+    cout << square_P << endl;
+    cout << square_S << endl;
     return 0;
 
 }
